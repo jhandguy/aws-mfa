63902ee83812e21509cba23d1f1498fca921f734
Eldad Zack <eldad.zack@egym.com>
2022-08-31 10:21:32 +0200

    chore: use thiserror and anyhow to improve error handling

--- src/error.rs
+++ src/error.rs
@@ -1,48 +1,16 @@
-use std::error::Error;
-use std::fmt::{Debug, Display, Formatter};
+use std::fmt::Debug;
 use std::io::Error as IOError;
+use thiserror::Error;
 
 use aws_sdk_sts::error::{GetCallerIdentityError, GetSessionTokenError};
 use aws_sdk_sts::types::SdkError;
 
-pub struct AwsAuthError {
-    message: String,
-}
-
-impl Error for AwsAuthError {}
-
-impl Display for AwsAuthError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.message)
-    }
-}
-
-impl Debug for AwsAuthError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.message)
-    }
-}
-
-impl From<IOError> for AwsAuthError {
-    fn from(e: IOError) -> Self {
-        Self {
-            message: format!("Failed to perform I/O operation: {}", e),
-        }
-    }
-}
-
-impl From<SdkError<GetCallerIdentityError>> for AwsAuthError {
-    fn from(e: SdkError<GetCallerIdentityError>) -> Self {
-        Self {
-            message: format!("Failed to get caller identity: {}", e),
-        }
-    }
-}
-
-impl From<SdkError<GetSessionTokenError>> for AwsAuthError {
-    fn from(e: SdkError<GetSessionTokenError>) -> Self {
-        Self {
-            message: format!("Failed to get session token: {}", e),
-        }
-    }
+#[derive(Error, Debug)]
+pub enum AwsAuthError {
+    #[error("Failed to perform I/O operation")]
+    IO(#[from] IOError),
+    #[error("Failed to get caller identity")]
+    CallerIdentity(#[from] SdkError<GetCallerIdentityError>),
+    #[error("Failed to get session token")]
+    SessionToken(#[from] SdkError<GetSessionTokenError>),
 }
